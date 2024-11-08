81ebf83e0b49b71333f267a7c37d33bc4e7bf962
RGBDeehan <rageebyasardeehan@gmail.com>
2024-11-08 22:35:13 +0600

    Add files via upload

---
+++ TJCW3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main (){
+float x , y ;
+    printf("Enter the value of x:");
+    scanf("%f",&x);
+
+    printf("Enter the value of y:");
+    scanf("%f",&y);
+
+    if ( x > 0 && y > 0) {
+        printf("The point is situated in 1st quardent");
+    }
+    else if( x < 0 && y > 0) {
+        printf("The point is situated in 2nd quardent");
+    }
+    else if ( x < 0 && y < 0) {
+        printf("The point is situated in 3rd quardent");
+    }
+    else if ( x > 0 && y < 0) {
+        printf("The point is situated in 4th quardent");
+    }
+    else if ( y == 0 && (x < 0 || x > 0)){
+        printf("The point is situated on X axis");
+    }
+    else if ( x == 0 && (y < 0 || y > 0)){
+        printf("The point is situated on Y axis");
+    }
+    else {printf("The point is ORIGIN");}
+
+
+return 0;
+}
